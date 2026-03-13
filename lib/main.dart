61af6852074a13ed582e85416e663430bb2cdc18
SupakitMark <6731503122@lamduan.mfu.ac.th>
2026-03-13 14:32:33 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,17 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(SmartClassApp());
 }
 
-class MyApp extends StatelessWidget {
+class SmartClassApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      body: Center(
-        child: Text('Hello, World!'),
-      ),
+      title: 'Smart Class',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeScreen(),
     );
   }
 }
